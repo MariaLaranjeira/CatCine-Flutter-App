d3b867e3592f395a4b702d59a636f2189a9e6d10
MariaLaranjeira <mlriblar@gmail.com>
2023-03-13 21:39:48 +0000

    Set Api to print to console

--- lib/api.dart
+++ lib/api.dart
@@ -1,30 +1,20 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-import 'media.dart';
+Future<void> main() async { // Make the main function asynchronous
+  final client = http.Client();
+  final query = "The Good Place"; // Replace with your desired query string
 
+  final request = http.Request('GET',
+      Uri.parse('https://mdblist.p.rapidapi.com/?s=$query'))
+    ..headers.addAll({
+      'X-RapidAPI-Key': 'd681c42be1msha8325a6eba62bd0p10fe15jsn10d36805b704',
+      'X-RapidAPI-Host': 'mdblist.p.rapidapi.com'
+    });
 
-class Api {
+  final streamedResponse = await client.send(request);
+  final response = await http.Response.fromStream(streamedResponse);
 
-  String? title;
-  Api (this.title);
-
-  Future<String> getMedia() async {
-    final client = http.Client();
-    //final title = "The Good Place"; // Replace with your desired query string
-
-    final request = http.Request('GET',
-        Uri.parse('https://mdblist.p.rapidapi.com/?s=$title'))
-      ..headers.addAll({
-        'X-RapidAPI-Key': 'd681c42be1msha8325a6eba62bd0p10fe15jsn10d36805b704',
-        'X-RapidAPI-Host': 'mdblist.p.rapidapi.com'
-      });
-
-    final streamedResponse = await client.send(request);
-    final response = await http.Response.fromStream(streamedResponse);
-
-    return response.body; // Return the response body as a string
-  }
-
+  print(response.body);
 }
 
