eebd5e161502d0fcc6b5b2b3f99aea31b7cd49f4
MariaLaranjeira <92693155+MariaLaranjeira@users.noreply.github.com>
2023-05-23 03:17:44 +0100

    Started implementing user media rating

--- lib/Pages/mediaPage.dart
+++ lib/Pages/mediaPage.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import '../Model/media.dart';
 import '../main.dart';
 
@@ -15,6 +17,15 @@ class MediaPage extends StatefulWidget {
 
 class _MediaPageState extends State<MediaPage> {
 
+  Map<int,Color> colors = {};
+
+  double _ratingValue = 0;
+
+  bool is1Pressed = false;
+  bool is2Pressed = false;
+  bool is3Pressed = false;
+  bool is4Pressed = false;
+  bool is5Pressed = false;
 
   getPosterURL() {
     if (widget.media.coverUrl != '') {
@@ -30,9 +41,36 @@ class _MediaPageState extends State<MediaPage> {
     return const AssetImage('images/catIcon.png');
   }
 
+  CollectionReference userDB = FirebaseFirestore.instance.collection('users');
+
+
+  loadUserRating() async{
+    await userDB
+        .doc(FirebaseAuth.instance.currentUser!.displayName!)
+        .collection('interacted_media')
+        .doc(widget.media.id)
+        .get()
+        .then((DocumentSnapshot documentSnapshot) async {
+      if (documentSnapshot.exists) {
+        var data = documentSnapshot.data();
+        var res = data as Map<String, dynamic>;
+        _ratingValue = res['rating_given'];
+      }
+    });
+  }
+
+  storeUserRating() async {
+    var usermedia = userDB.doc(FirebaseAuth.instance.currentUser!.displayName!).collection('interacted_media').doc(widget.media.id);
+    await usermedia.set({
+      'rating_given': _ratingValue,
+    }, SetOptions(merge: true));
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       backgroundColor: const Color(0xff393d5a),
@@ -118,6 +156,26 @@ class _MediaPageState extends State<MediaPage> {
                 fontSize: 18,
               ),
             ),
+            RatingBar(
+                initialRating: 0,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                ratingWidget: RatingWidget(
+                    full: const Icon(Icons.star, color: Colors.yellow),
+                    half: const Icon(
+                      Icons.star_half,
+                      color: Colors.yellow,
+                    ),
+                    empty: const Icon(
+                      Icons.star_outline,
+                      color: Colors.yellow,
+                    )),
+                onRatingUpdate: (value) {
+                  setState(() {
+                    _ratingValue = value;
+                  });
+                }),
           ],
         ),
       ),
